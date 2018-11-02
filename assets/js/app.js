886850f3a240e65f554ec530845d91b05a84994e
Tristan McDonald <tristan@permadesign.co.uk>
2018-11-02 15:23:51 +0000

    Add polyfill for foreach loops on nodelists in IE11

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
 // interpolate between minimum ratio & maximum ratio, based on current viewport width
 const TypeRatioInterpolation = require("./modules/type-ratio-interpolation");
 TypeRatioInterpolation.init();
+
+// polyfill to enable us to use forEach on node lists in IE11
+const ForEachPolyfill = require("./modules/foreach-polyfill");
+ForEachPolyfill.init();
