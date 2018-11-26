eb347eaa4ae6852027d849e1253ce1e27ff32cb1
Tristan McDonald <6652472+trubblebruin@users.noreply.github.com>
2018-11-26 21:27:19 +0000

    Use strict mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // interpolate between minimum ratio & maximum ratio, based on current viewport width
 const TypeRatioInterpolation = require("./modules/type-ratio-interpolation");
 TypeRatioInterpolation.init();
