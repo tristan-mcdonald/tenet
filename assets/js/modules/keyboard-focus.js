0350f50544bb369de7c919210808ab8531c243d3
Tristan McDonald <tristan@permadesign.co.uk>
2020-07-09 15:59:39 +0100

    Add accessibility mixin, use doublequotes

--- assets/js/modules/keyboard-focus.js
+++ assets/js/modules/keyboard-focus.js
@@ -1,6 +1,7 @@
 module.exports = {
     init: () => {
         /* global ally */
+        console.log("loaded version", ally.version, "of a11y.js");
         // detect focus source using a11y.js
         const focusSource = ally.style.focusSource(); // eslint-disable-line no-unused-vars
     },
