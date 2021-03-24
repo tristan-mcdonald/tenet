bdee8ee701847f3a961b2fc7a66ed25ad73a082a
trubblebruin <letswork@tristanmcdonald.co.uk>
2021-03-24 12:25:45 +0000

    Hard code interpolation function name

--- source/build_assets/js/modules/type_ratio_interpolation.js
+++ source/build_assets/js/modules/type_ratio_interpolation.js
@@ -2,23 +2,23 @@ function scaleRatio (currentViewportWidth, minimumValue, maximumValue) {
     // store the minimum viewport width at which `--ratio`
     // custom property will be set. this must match values
     // set in `breakpoints.styl` & `typography.styl`.
-    const minRange            = 480;
+    const minRange = 480;
     // store the maximum viewport width at which `--ratio`
     // custom property will be set. this must match values
     // set in `breakpoints.styl` & `typography.styl`.
-    const maxRange            = 1680;
+    const maxRange = 1680;
     // calculate and store the rate at wich our ratio changes
     // when the viewport width changes by 1px
-    const rateOfChange        = (maximumValue - minimumValue) / (maxRange - minRange);
+    const rateOfChange = (maximumValue - minimumValue) / (maxRange - minRange);
     // store the intercept (the value of Y when all X=0)
     const valueAtViewportZero = minimumValue - rateOfChange * minRange;
     // if we follow a linear progression from ratio m at viewport M,
     // to ratio n at viewport N, calculate and store the ratio at viewport X
-    const interpolatedRatio   = currentViewportWidth * rateOfChange + valueAtViewportZero;
+    const interpolatedRatio = currentViewportWidth * rateOfChange + valueAtViewportZero;
     // use `interpolatedRatio` value if it's within range.
     // if it would be smaller than the smallest acceptable ratio, use the smallest.
     // if it would be larger than the largest acceptable ratio, use the largest.
-    const boundedRatio          = Math.max(minimumValue, Math.min(interpolatedRatio, maximumValue));
+    const boundedRatio = Math.max(minimumValue, Math.min(interpolatedRatio, maximumValue));
     // store final ratio in CSS custom property `--ratio`
     document.documentElement.style.setProperty("--ratio", boundedRatio);
 }
