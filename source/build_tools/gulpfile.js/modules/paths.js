92ee6aece057e40ec2e0a57d87f68629446bdb2c
tristan-mcdonald <letswork@tristanmcdonald.co.uk>
2023-03-29 13:45:35 +0100

    Rename source folders for easier command line autocompletion

--- source/build_tools/gulpfile.js/modules/paths.js
+++ source/build_tools/gulpfile.js/modules/paths.js
@@ -5,27 +5,27 @@
 module.exports = {
     javascript: {
         config: "./.eslintrc",
-        input: "../build_assets/js/app.js",
-        lint: "../build_assets/js/**/*.js",
+        input: "../assets/js/app.js",
+        lint: "../assets/js/**/*.js",
         output: "../../distribution/assets/js/",
         transpiled: "../../distribution/assets/js/app.js",
         uglified: "../../distribution/assets/js/app.min.js",
-        watch: "../build_assets/js/**/*.js",
+        watch: "../assets/js/**/*.js",
     },
     images: {
-        css: "../build_assets/images",
+        css: "../assets/images",
     },
     server: {
         root: "../../distribution/assets/reference",
     },
     stylus: {
         config: ".stylintrc",
-        input: "../build_assets/stylus/app.styl",
-        lint: "../build_assets/stylus/**/*.styl",
+        input: "../assets/stylus/app.styl",
+        lint: "../assets/stylus/**/*.styl",
         output: "../../distribution/assets/css/",
         transpiled: "../../distribution/assets/css/app.css",
         uglified: "../../distribution/assets/css/app.min.css",
-        watch: "../build_assets/stylus/**/*.styl",
+        watch: "../assets/stylus/**/*.styl",
     },
     templates: {
         input_file: "../reference/templates/index.njk",
